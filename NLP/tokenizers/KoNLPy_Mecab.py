08e1bd3907fa75f3c665e028e39fcc49679de602
Dong-geon Lee <peter8739@gmail.com>
2023-05-03 19:51:20 +0900

    예시 문장 추가

--- NLP/tokenizers/KoNLPy_Mecab.py
+++ NLP/tokenizers/KoNLPy_Mecab.py
@@ -74,3 +74,19 @@ if __name__=="__main__":
     Tokenized essential POS: ['이거', '실화', '진짜', '가슴', '내', '생각', '그', '나투', '사스케', '맞', '정말', '세계관', '최강', '자', '들', '싸움']
     Tokenized only Nouns: ['이거', '실화', '가슴', '내', '생각', '나투', '사스케', '세계관', '최강', '싸움']
     """
+    
+    
+    text = "XX 년 8 월 0000 편 제주에서 김포로 가기 위하여 00:00 분경 FL250 에 도달하여 순항 중 광주북쪽지역에 심한 thunderstorm 이 발생하여 인천 control 에 우측으로 10NM deviation request 하였음."
+    print(f"Original Text: {text}")
+    print(f"Tokenized Text: {tokenizer(text)}")
+    print(f"Tokenized except Josa: {josaless_tokenizer(text, josa_list)}")
+    print(f"Tokenized essential POS: {essential_tokenizer(text, essential_list)}")
+    print(f"Tokenized only Nouns: {noun_tokenizer(text)}")
+    
+    """
+    Original Text: XX 년 8 월 0000 편 제주에서 김포로 가기 위하여 00:00 분경 FL250 에 도달하여 순항 중 광주북쪽지역에 심한 thunderstorm 이 발생하여 인천 control 에 우측으로 10NM deviation request 하였음.
+    Tokenized Text: ['XX', '년', '8', '월', '0000', '편', '제주', '에서', '김포', '로', '가', '기', '위하', '여', '0000', '분경', 'FL', '250', '에', '도달', '하', '여', '순항', '중', '광주', '북쪽', '지역', '에', '심한', 'thunderstorm', '이', '발생', '하', '여', '인천', 'control', '에', '우측', '으로', '10', 'NM', 'deviation', 'request', '하', '였', '음']
+    Tokenized except Josa: ['XX', '년', '8', '월', '0000', '편', '제주', '김포', '가', '기', '위하', '여', '0000', '분경', 'FL', '250', '도달', '하', '여', '순항', '중', '광주', '북쪽', '지역', '심한', 'thunderstorm', '발생', '하', '여', '인천', 'control', '우측', '10', 'NM', 'deviation', 'request', '하', '였', '음']
+    Tokenized essential POS: ['XX', '년', '8', '월', '0000', '편', '제주', '김포', '가', '위하', '0000', '분경', 'FL', '250', '도달', '순항', '중', '광주', '북쪽', '지역', 'thunderstorm', '발생', '인천', 'control', '우측', '10', 'NM', 'deviation', 'request']
+    Tokenized only Nouns: ['년', '월', '편', '제주', '김포', '분경', '도달', '순항', '중', '광주', '북쪽', '지역', '발생', '인천', '우측']
+    """
